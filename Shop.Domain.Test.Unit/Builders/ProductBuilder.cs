21d2a86de190de7ad590f7835cece1a21f01b568
Meharn <jam@gmaile.com>
2023-10-20 21:07:01 +0330

    Product Tests

---
+++ Shop.Domain.Test.Unit/Builders/ProductBuilder.cs
@@ -0,0 +1,32 @@
+﻿using Shop.Domain.ProductAgg;
+using Shop.Domain.Products;
+using Shop.Domain.Shared;
+using System.Collections.Generic;
+
+namespace Shop.Domain.Test.Unit.Builders;
+
+internal class ProductBuilder : IDisposable
+{
+    private string _title = "test";
+    private Money _money = new Money(1000000);
+
+    public ProductBuilder SetTitle(string title)
+    {
+        _title = title;
+        return this;
+    }
+    public ProductBuilder SetMoney(int rialPrice)
+    {
+        _money = new Money(rialPrice);
+        return this;
+    }
+    public Product Build()
+    {
+        return new Product(_title, _money);
+    }
+
+    public void Dispose()
+    {
+        // Tear down
+    }
+}
